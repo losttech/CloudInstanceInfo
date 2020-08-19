b6fd3ba0bb14b075f9107e112b7596e597de31c9
Victor Milovanov <lost@losttech.software>
2020-08-19 13:38:59 -0700

    added a simple command line program, that prints current instance info (untested)

---
+++ app/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LostTech.Cloud {
+    using System;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+
+    class Program {
+        static async Task Main() {
+            var provider = new AnyCloudInstanceInfoProvider();
+            var info = await provider.GetInstanceInfoAsync();
+            string json = JsonSerializer.Serialize(info, jsonSerializerOptions);
+            Console.WriteLine(json);
+        }
+
+        static readonly JsonSerializerOptions jsonSerializerOptions = new JsonSerializerOptions {
+            WriteIndented = true,
+        };
+    }
+}
